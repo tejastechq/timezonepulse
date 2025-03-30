70b5ab662828910c40db5024f978268ec84ced0e
Tejas PC <tejashaveridev@gmail.com>
2025-03-29 21:22:18 -0500

    fix: Update trusted-types policy to fix type error

--- lib/utils/trusted-types.ts
+++ lib/utils/trusted-types.ts
@@ -4,7 +4,7 @@ if (typeof window !== 'undefined') {
   // Create default trusted types policy
   if (window.trustedTypes) {
     window.trustedTypes.createPolicy('default', {
-      createHTML: (string) => DOMPurify.sanitize(string, { RETURN_TRUSTED_TYPE: true }),
+      createHTML: (string) => DOMPurify.sanitize(string) as string,
       createScript: (string) => string,
       createScriptURL: (string) => string
     });
